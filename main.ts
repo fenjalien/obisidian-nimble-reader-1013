ac6b5c123af68f48b6d4d76f41a33bffd8ecae0a
Jack <34489450+fenjalien@users.noreply.github.com>
2023-03-09 03:52:29 +0000

    init commit working in read mode

--- main.ts
+++ main.ts
@@ -1,13 +1,30 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownPostProcessorContext, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	lineHeight: number,
+	saccadesColor: string,
+	saccadesStyle: string,
+	saccadesInterval: number,
+	fixationStrength: number,
+	fixationEdgeOpacity: number,
+	maxFixationParts: number,
+	fixationLowerBound: number,
+	brWordStemPercentage: number,
+
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	lineHeight: 1,
+	saccadesColor: "",
+	saccadesStyle: 'bold-600',
+	saccadesInterval: 0,
+	fixationStrength: 2,
+	fixationEdgeOpacity: 80,
+	maxFixationParts: 4,
+	fixationLowerBound: 0,
+	brWordStemPercentage: 0.7
 }
 
 export default class MyPlugin extends Plugin {
@@ -16,66 +33,11 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.registerMarkdownPostProcessor((el, ctx) => this.parseElement(el));
+		console.log("loaded jiffy");
+		document.body.setAttribute('br-mode', 'on');
+		this.updateStyles();
 	}
 
 	onunload() {
@@ -89,23 +51,97 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	parseElement(element: HTMLElement) {
+		console.log("firing");
+		this.parseNodes(element);
+	}
+
+	parseNodes(node: Node) {
+		if (node.nodeType === Node.TEXT_NODE && node.nodeValue?.length) {
+			const brSpan = document.createElement("br-span");
+			brSpan.innerHTML = this.highlightText(node.nodeValue);
+			if (brSpan.childElementCount === 0) return;
+
+			// to avoid duplicates of brSpan, check it if
+			// this current textNode has a left sibling of br span
+			// we know that is possible because
+			// we will specifically insert the br-span
+			// on the left of a text node, and keep
+			// the text node alive later. so if we get to
+			// this text node again. that means that the
+			// text node was updated and the br span is now stale
+			// so remove that if exist
+			// if (node.previousSibling?.nodeName === 'BR-SPAN') {
+			// 	node.parentElement!.removeChild(node.previousSibling);
+			// }
+
+			// dont replace for now, cause we're keeping it alive
+			// below
+			node.parentElement!.replaceChild(brSpan, node);
+
+			// keep the textNode alive in the dom, but
+			// empty it's contents
+			// and insert the brSpan just before it
+			// we need the text node alive because
+			// youtube has some reference for it internally
+			// and we want to listen to it when it changes
+			// node.parentElement!.insertBefore(brSpan, node);
+			// node.textContent = '';
+		}
+
+		if (node.hasChildNodes()) node.childNodes.forEach((n) => this.parseNodes(n));
+	}
+
+	highlightText(sentenceText: string) {
+		return sentenceText.replace(/\p{L}+/gu, (word) => {
+			const { length } = word;
+
+			const brWordStemWidth = length > 3 ? Math.round(length * this.settings.brWordStemPercentage) : length;
+
+			const firstHalf = word.slice(0, brWordStemWidth);
+			const secondHalf = word.slice(brWordStemWidth);
+			const htmlWord = `<br-bold>${this.makeFixations(firstHalf)}</br-bold>${secondHalf.length ? `<br-edge>${secondHalf}</br-edge>` : ''}`;
+			return htmlWord;
+		});
+	}
+
+	makeFixations(textContent: string) {
+		const COMPUTED_MAX_FIXATION_PARTS = textContent.length >= this.settings.maxFixationParts ? this.settings.maxFixationParts : textContent.length;
+
+		const fixationWidth = Math.ceil(textContent.length * (1 / COMPUTED_MAX_FIXATION_PARTS));
+
+		if (fixationWidth === this.settings.fixationLowerBound) {
+			return `<br-fixation fixation-strength="1">${textContent}</br-fixation>`;
+		}
+
+		const fixationsSplits = new Array(COMPUTED_MAX_FIXATION_PARTS).fill(null).map((item, index) => {
+			const wordStartBoundary = index * fixationWidth;
+			const wordEndBoundary = wordStartBoundary + fixationWidth > textContent.length ? textContent.length : wordStartBoundary + fixationWidth;
+
+			return `<br-fixation fixation-strength="${index + 1}">${textContent.slice(wordStartBoundary, wordEndBoundary)}</br-fixation>`;
+		});
+
+		return fixationsSplits.join('');
+	}
+
+	updateStyles() {
+		document.body.style.setProperty("--fixation-edge-opacity", this.settings.fixationEdgeOpacity.toString() + "%");
+		document.body.style.setProperty("--br-line-height", this.settings.lineHeight.toString());
+
+		let bold, lineStyle;
+		if (this.settings.saccadesStyle.contains("bold")) {
+			[, bold] = this.settings.saccadesStyle.split("-");
+			lineStyle = "";
+		} else {
+			[lineStyle] = this.settings.saccadesStyle.split("-");
+			bold = "";
+		}
+		document.body.style.setProperty("--br-boldness", bold);
+		document.body.style.setProperty("--br-line-style", lineStyle);
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
 
 class SampleSettingTab extends PluginSettingTab {
 	plugin: MyPlugin;
@@ -116,22 +152,22 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		// const { containerEl } = this;
 
-		containerEl.empty();
+		// containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		// containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		// new Setting(containerEl)
+		// 	.setName('Setting #1')
+		// 	.setDesc('It\'s a secret')
+		// 	.addText(text => text
+		// 		.setPlaceholder('Enter your secret')
+		// 		.setValue(this.plugin.settings.mySetting)
+		// 		.onChange(async (value) => {
+		// 			console.log('Secret: ' + value);
+		// 			this.plugin.settings.mySetting = value;
+		// 			await this.plugin.saveSettings();
+		// 		}));
 	}
 }
